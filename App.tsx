669b7f56a3dc3a355c69ab03e85cda8a305172a1
Mustapha BARKI <code4yo@gmail.com>
2025-06-21 04:07:13 +0200

    Create App.tsx
    Signed-off-by: Mustapha BARKI <code4yo@gmail.com>

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './src/store/store';
+import AppNavigator from './src/navigation/AppNavigator';
+
+export default function App() {
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <AppNavigator />
+      </PersistGate>
+    </Provider>
+  );
+}
