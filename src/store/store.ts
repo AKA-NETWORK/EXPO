ab228a964e7d0d429343bbdcdadcd6dd6d0cf780
Mustapha BARKI <code4yo@gmail.com>
2025-06-21 04:04:10 +0200

    Create store.ts
    Signed-off-by: Mustapha BARKI <code4yo@gmail.com>

---
+++ src/store/store.ts
@@ -0,0 +1,32 @@
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import authReducer from './slices/authSlice';
+
+// Persist config
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+  whitelist: ['auth'], // Only persist auth state
+};
+
+const rootReducer = combineReducers({
+  auth: authReducer,
+  // Add other reducers here
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false, // Disable for redux-persist
+    }),
+});
+
+export const persistor = persistStore(store);
+
+// TypeScript types
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
