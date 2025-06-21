8b5edd2852223efbce3a816695d8eb22d7dbd702
Mustapha BARKI <code4yo@gmail.com>
2025-06-21 04:19:50 +0200

    Create ThemeContext.tsx
    Signed-off-by: Mustapha BARKI <code4yo@gmail.com>

---
+++ src/contexts/ThemeContext.tsx
@@ -0,0 +1,36 @@
+import React, { createContext, useContext, useState } from 'react';
+import { lightTheme, darkTheme } from '../constants/themes';
+
+type ThemeType = 'light' | 'dark';
+
+interface ThemeContextProps {
+  theme: ThemeType;
+  toggleTheme: () => void;
+  colors: typeof lightTheme;
+}
+
+const ThemeContext = createContext<ThemeContextProps | undefined>(undefined);
+
+export const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
+  const [theme, setTheme] = useState<ThemeType>('light');
+
+  const toggleTheme = () => {
+    setTheme(prev => (prev === 'light' ? 'dark' : 'light'));
+  };
+
+  const colors = theme === 'light' ? lightTheme : darkTheme;
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme, colors }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export const useTheme = () => {
+  const context = useContext(ThemeContext);
+  if (!context) {
+    throw new Error('useTheme must be used within a ThemeProvider');
+  }
+  return context;
+};
